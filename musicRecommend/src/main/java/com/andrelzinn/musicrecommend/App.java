42b20f83c02af4863c4d28a5e91508dc72f2d54e
andrelzinn <andrellira85@gmail.com>
2025-08-01 21:20:52 -0300

    first commit

---
+++ musicRecommend/src/main/java/com/andrelzinn/musicrecommend/App.java
@@ -0,0 +1,13 @@
+package com.andrelzinn.musicrecommend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
